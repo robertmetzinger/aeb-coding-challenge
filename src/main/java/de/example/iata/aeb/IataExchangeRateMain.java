d0ec846f6c3c77b2de8c8965ce6b88b43f0731fb
robertmetzinger <robert.metzinger@web.de>
2024-02-25 14:04:24 +0000

    Initial commit

---
+++ src/main/java/de/example/iata/aeb/IataExchangeRateMain.java
@@ -0,0 +1,10 @@
+package de.example.iata.aeb;
+
+import de.example.iata.aeb.IataExchangeRateApplication;
+
+public class IataExchangeRateMain {
+
+	public static void main(String[] args) throws Exception {
+		new IataExchangeRateApplication().run();
+	}
+}
