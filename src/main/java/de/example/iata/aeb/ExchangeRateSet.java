22d36a95980605792503398dc5392a29786250f9
robertmetzinger <robert.metzinger@web.de>
2024-02-26 00:06:19 +0000

    add check for duplicates

--- src/main/java/de/example/iata/aeb/ExchangeRateSet.java
+++ src/main/java/de/example/iata/aeb/ExchangeRateSet.java
@@ -12,13 +12,40 @@ public class ExchangeRateSet {
     }
 
     public boolean addExchangeRate(ExchangeRate exchangeRate) {
-        boolean success = false;
+        if (isDuplicate(exchangeRate)) {
+            return false;
+        }
         exchangeRates.add(exchangeRate);
-        return success;
+        return true;
+    }
+
+    private List<ExchangeRate> getExchangeRatesByCurrencyIsoCode(String currencyIsoCode) {
+        List<ExchangeRate> exchangeRatesWithIsoCode = new ArrayList<>();
+        for (ExchangeRate exchangeRate : exchangeRates) {
+            if (exchangeRate.getCurrencyIsoCode().equals(currencyIsoCode)) {
+                exchangeRatesWithIsoCode.add(exchangeRate);
+            }
+        }
+        return exchangeRatesWithIsoCode;
+    }
+
+    private boolean isDuplicate(ExchangeRate exchangeRate) {
+        List<ExchangeRate> exchangeRatesWithIsoCode = getExchangeRatesByCurrencyIsoCode(exchangeRate.getCurrencyIsoCode());
+        for (ExchangeRate exchangeRateOfIteration : exchangeRatesWithIsoCode) {
+            if (exchangeRateOfIteration.getStartValidDate().isEqual(exchangeRate.getStartValidDate())) {
+                if (exchangeRateOfIteration.getEndValidDate().isEqual(exchangeRate.getEndValidDate())) {
+                    System.out.print("Duplicate ignored ");
+                    exchangeRate.print();
+                    System.out.println();
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void print() {
-        for (ExchangeRate exchangeRate: exchangeRates) {
+        for (ExchangeRate exchangeRate : exchangeRates) {
             exchangeRate.print();
             System.out.println();
         }
